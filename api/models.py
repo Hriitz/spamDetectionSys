754d405a2b84c19d23bfa2d9f82ecebb15c445fd
Hritik Singh <89767461+Hriitz@users.noreply.github.com>
2023-11-25 16:00:40 +0530

    bug fixes

--- api/models.py
+++ api/models.py
@@ -14,8 +14,8 @@ class CustomUserManager(BaseUserManager):
         return user
 
     def create_superuser(self, phone_number, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
+        # extra_fields.setdefault('is_staff', True)
+        # extra_fields.setdefault('is_superuser', True)
 
         return self.create_user(phone_number, password, **extra_fields)
 
